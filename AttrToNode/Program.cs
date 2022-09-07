643bd9563b44a927cf2bdb9e97deb8518c438fec
Mohd Mubsshir Khan <kmubsshir@hotmail.com>
2022-09-07 20:29:58 +0530

    Add project files.

---
+++ AttrToNode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AttrToNode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // select the path 
+            string path = @"C:\Users\Mubsshir\source\repos\AttrToNode\AttrToNode\Data.xml";
+            Console.WriteLine(ReadXML.Fn_ReadXML(path));
+        }
+    }
+}
